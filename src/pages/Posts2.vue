e9d6726ed36fb424be7c3ed7f9dba38026ea54a2
jwchan1996 <741755613@qq.com>
2021-02-22 23:44:24 +0800

    feat: 页面 GraphQL 查询静态渲染与动态渲染

---
+++ src/pages/Posts2.vue
@@ -0,0 +1,43 @@
+<template>
+  <Layout>
+    <div>
+      <h1>Posts2</h1>
+      <ul>
+        <li v-for="edge in $page.posts.edges" :key="edge.node.id">
+          <g-link :to="edge.node.path">{{ edge.node.title }}</g-link>
+        </li>
+      </ul>
+    </div>
+  </Layout>
+</template>
+
+<page-query>
+query {
+  posts: allPost {
+    edges {
+      node {
+        id
+        title
+      }
+    }
+  }
+}
+</page-query>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.pager .active {
+  color: #fff;
+  background-color: #007bff;
+  border-color: #007bff;
+}
+
+.pager .page-link {
+  display: inline;
+}
+</style>
